3c3f601a80829194e61e79a62b3c1610770de6f8
zhixiong <lxsbw@126.com>
2021-10-03 00:33:52 +0800

    mongodb debug config

--- config/config.default.js
+++ config/config.default.js
@@ -2,11 +2,11 @@
  * @Author: zhixiong.fu
  * @Date: 2020-12-21 21:05:52
  * @Last Modified by: zhixiong.fu
- * @Last Modified time: 2021-09-26 20:37:53
+ * @Last Modified time: 2021-10-03 00:29:32
  */
 
 module.exports = {
-  mongodb: {
+  mongodbConfig: {
     user: 'test_user',
     pass: '123456',
     host: '121.37.188.31',
