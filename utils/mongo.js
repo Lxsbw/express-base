33f9d57aad9da95ff87c7561fc158b902844bda9
zhixiong <lxsbw@outlook.com>
2025-05-04 23:00:10 +0800

    update package

--- utils/mongo.js
+++ utils/mongo.js
@@ -11,11 +11,11 @@ import { mongodbConfig, env } from '../config/config.default.js';
  */
 function getMongodbConfig() {
   let options = {
-    useCreateIndex: true,
-    poolSize: 5, // 连接池中维护的连接数
-    useNewUrlParser: true,
+    // useCreateIndex: true,
+    maxPoolSize: 5, // 连接池中维护的连接数
+    // useNewUrlParser: true,
     autoIndex: false,
-    useUnifiedTopology: true
+    // useUnifiedTopology: true
     // keepAlive: 120,
   };
   return options;
