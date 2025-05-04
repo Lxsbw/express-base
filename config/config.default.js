f7db8e01496d109dd24a25d5448c273c579966ec
zhixiong <lxsbw@outlook.com>
2025-05-04 22:00:51 +0800

    update linq version

--- config/config.default.js
+++ config/config.default.js
@@ -2,15 +2,15 @@
  * @Author: zhixiong.fu
  * @Date: 2020-12-21 21:05:52
  * @Last Modified by: zhixiong.fu
- * @Last Modified time: 2023-03-06 16:26:15
+ * @Last Modified time: 2025-05-04 21:57:16
  */
 
 module.exports = {
   mongodbConfig: {
     user: 'test_user',
     pass: '123456',
-    host: '10.211.55.58',
-    port: '16380',
+    host: '10.211.55.4',
+    port: '16390',
     db: 'testdb'
   },
   env: {
